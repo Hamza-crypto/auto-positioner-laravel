95d6a60eefd0d00694ef3b4d9280d0b4f6d36082
mrSaadZulfiqar <mr.saadzulfiqar@gmail.com>
2023-09-20 07:16:59 +0500

    fixed bundling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,24 +57,24 @@
     </div>
 
     {{-- Bundle scripts --}}
-    <script src="{{ asset('assets/js/bundle.js') }}"></script>
+    <script src="{{ mix('assets/js/bundle.js') }}"></script>
 
     {{-- place onload bundling scripts here  --}}
 
     @yield("bundlingScripts")
-
+ 
     {{-- Slick --}}
     <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 
     {{-- Examples --}}
-    <script src="{{ asset('assets/js/dashboard.js') }}"></script>
+    <script src="{{ mix('assets/js/dashboard.js') }}"></script>
 
     <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.18/dist/sweetalert2.min.css" rel="stylesheet">
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.18/dist/sweetalert2.all.min.js"></script>
 
     {{-- Main Javascript file --}}
-    <script src="{{ asset('assets/js/app_min.js') }}"></script>
+    <script src="{{ mix('assets/js/app_min.js') }}"></script>
 
 
 
